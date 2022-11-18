e6fdab3a247c5fa9ee59fa6b9dd1f3fcc8e02d0e
steelhelmet1 <egyman2010@gmail.com>
2022-11-18 19:53:36 +0200

    created pages and layout

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,6 +12,9 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/contact" page={ContactPage} name="contact" />
+      <Route path="/about" page={AboutPage} name="about" />
+      <Route path="/home" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
---
+++ web/src/layouts/BaseLayout/BaseLayout.stories.js
@@ -0,0 +1,10 @@
+import BaseLayout from './BaseLayout'
+
+export const generated = (args) => {
+  return <BaseLayout {...args} />
+}
+
+export default {
+  title: 'Layouts/BaseLayout',
+  component: BaseLayout,
+}
