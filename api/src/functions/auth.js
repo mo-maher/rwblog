b711ef197da0d2c31c6eb7cd4839ad41ed482fa9
steelhelmet1 <egyman2010@gmail.com>
2022-11-19 08:57:07 +0200

    test deploy

--- api/src/functions/auth.js
+++ api/src/functions/auth.js
@@ -104,6 +104,7 @@ export const handler = async (event, context) => {
     // `signUp()` function in the form of: `{ message: 'String here' }`.
     handler: ({ username, hashedPassword, salt, userAttributes }) => {
       return false
+
       //  db.user.create({
       //   data: {
       //     email: username,
